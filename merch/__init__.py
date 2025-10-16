4b6cdaad8ef5deadc2e10567e65f0259acb09ba0
Nico Cava <nmcava01@gmail.com>
2025-10-16 15:06:43 -0400

    added error handlers and pages

--- merch/__init__.py
+++ merch/__init__.py
@@ -3,5 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 from merch.core.views import core
+from merch.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
